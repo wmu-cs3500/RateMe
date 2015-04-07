167e317ff5bd82c70b697011ad8c2fdd5a6ffc23
CS VS <wmu.cs3500@gmail.com>
2015-04-07 01:15:08 +0000

    modified:   config/routes.rb 	modified:   db/schema.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rateme::Application.routes.draw do
+  resources :users
+
   resources :assignments
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404163058) do
+ActiveRecord::Schema.define(version: 20150407011449) do
 
   create_table "assignments", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.date     "open"
     t.date     "close"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "username"
+    t.string   "password"
+    t.string   "email"
+    t.boolean  "admin_account"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
